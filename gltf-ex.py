095afcd0c45982bbc4373b11b1a37c10dd43b8e9
Daniel Moti <33625136+dbaltazar1@users.noreply.github.com>
2022-09-27 21:03:01 +0200

    Update gltf-ex.py

--- gltf-ex.py
+++ gltf-ex.py
@@ -1,15 +1,15 @@
 import bpy 
 import os
-bl_info={"name":"GLTF-EXport single batch files",
-	"description":"Exporter that exports single gltf files each one by one",
+bl_info={"name":"Batch-EXport single files",
+    "description":"Exporter that exports single files each one by one",
         "author": "Moti Daniel",  
-        "version":(1,0),
-	"blender": (3, 2, 1),
+        "version":(1,1),
+    "blender": (3, 2, 1),
         "location": "Sidebar shortcut N"}
 
 class gltft(bpy.types.Operator):
-    bl_idname = 'test.testgltf'
-    bl_label = 'gltftest'
+    bl_idname = 'test.gltftest'
+    bl_label = 'gltfba'
     bl_options = {'PRESET','UNDO'}
     def execute(self, context):
         bpy.ops.object.select_all(action='SELECT')
@@ -30,20 +30,45 @@ class gltft(bpy.types.Operator):
             obj.select_set(True)
         return {'FINISHED'}
 
+class objex(bpy.types.Operator):
+    bl_idname = 'test.objtest'
+    bl_label = 'objba'
+    bl_options = {'PRESET','UNDO'}
+    def execute(self, context):
+        bpy.ops.object.select_all(action='SELECT')
+        basedir = os.path.dirname(bpy.data.filepath)
+        view_layer = bpy.context.view_layer
+        obj_active = view_layer.objects.active
+        selection = bpy.context.selected_objects
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj in selection:
+            obj.select_set(True)
+            view_layer.objects.active = obj
+            name = bpy.path.clean_name(obj.name)
+            fn = os.path.join(basedir, name)
+            bpy.ops.export_scene.gltf(filepath=fn + ".obj", use_selection=True)
+            obj.select_set(False)
+        view_layer.objects.active = obj_active
+        for obj in selection:
+            obj.select_set(True)
+        return {'FINISHED'}
+
 class panel(bpy.types.Panel):
-    bl_label = 'GLTF Single Batch Exporter'
+    bl_label = 'Single Batch Exporter'
     bl_idname = 'P_PT_panel'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'GLTF EX'
+    bl_category = 'Batch EX'
     def draw(self, context):
         layout = self.layout
         obj = context.object
         row = layout.row()
         row.operator(gltft.bl_idname, text='EXport GLTF files')
+        row = layout.row()
+        row.operator(objex.bl_idname, text='EXport OBJ files')
 
 from bpy.utils import register_class, unregister_class
-_classes = [panel, gltft]
+_classes = [panel, gltft, objex]
 def register():
     for cls in _classes:
         register_class(cls)
